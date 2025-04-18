78abd72886787a615cf3b8b523948a0abdf22a0a
Yogesh-Kshirsagar <yogi.kshirsagar@gmail.com>
2025-04-18 07:51:57 +0530

    Implemented code to for identifying duplicate elements

---
+++ 01-Easy/013-Contains Duplicate/ContainsDuplicate.java
@@ -0,0 +1,28 @@
+package org.example.arrays.easy;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 1};
+        int[] nums2 = {1, 2, 3, 4};
+        int[] nums3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+
+        System.out.println(containsDuplicate(nums1)); // true
+        System.out.println(containsDuplicate(nums2)); // false
+        System.out.println(containsDuplicate(nums3)); // true
+    }
+}
