498d8435f0e8d3904e664e575f63e06f0ce2a929
Yogesh-Kshirsagar <yogi.kshirsagar@gmail.com>
2025-05-05 07:44:43 +0530

    Implemented the logic to return true or false to decide on placing the flowers.

---
+++ 01-Easy/036-Can Place Flowers/FlowerbedPlanting.java
@@ -0,0 +1,32 @@
+package com.study.com;
+
+public class FlowerbedPlanting {
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int len = flowerbed.length;
+
+        for (int i = 0; i < len; i++) {
+            if (flowerbed[i] == 0) {
+                boolean emptyLeft = (i == 0 || flowerbed[i - 1] == 0);
+                boolean emptyRight = (i == len - 1 || flowerbed[i + 1] == 0);
+
+                if (emptyLeft && emptyRight) {
+                    flowerbed[i] = 1; // plant a flower
+                    n--;
+                    if (n == 0) return true; // planted enough
+                }
+            }
+        }
+
+        return n <= 0;
+    }
+
+    public static void main(String[] args) {
+        int[] flowerbed1 = {1, 0, 0, 0, 1};
+        int n1 = 1;
+        System.out.println(canPlaceFlowers(flowerbed1, n1)); // true
+
+        int[] flowerbed2 = {1, 0, 0, 0, 1};
+        int n2 = 2;
+        System.out.println(canPlaceFlowers(flowerbed2, n2)); // false
+    }
+}
